045b1ef8c9c114cbabfd99ecc3e2c41531a4baa0
pxvnc1617 <chsong1617@nate.com>
2024-05-18 21:31:43 +0900

    [initial] initial commit

---
+++ src/test/java/com/example/bank_coding/BankCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
