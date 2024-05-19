10b7d04db882983b5a85f17ec890aefb6b801b00
pxvnc1617 <chsong1617@nate.com>
2024-05-19 09:32:34 +0900

    [feat]Security configuration

--- src/main/java/com/example/bank_coding/BankCodingApplication.java
+++ src/main/java/com/example/bank_coding/BankCodingApplication.java
@@ -2,14 +2,21 @@ package com.example.bank_coding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.parameters.P;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BankCodingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankCodingApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(BankCodingApplication.class, args);
+		String [] iocNames = context.getBeanDefinitionNames();
+		for (String name : iocNames) {
+			System.out.println(name);
+		}
 	}
 
 }
---
+++ src/main/java/com/example/bank_coding/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.bank_coding.config;
+
+import com.example.bank_coding.domain.user.UserRole;
+import org.slf4j.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        logger.debug("debug: BCryptPasswordEncoder!!!");
+        return new BCryptPasswordEncoder();
+    }
+
+    // JWT 서버를 위해 session 사용하지 않는다.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.debug("debug: SecurityFilterChain!!!");
+        http.headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()));
+        http.csrf(csrf -> csrf.disable());
+        http.cors(cors -> cors.configurationSource(configurationSource()));
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.formLogin(formLogin -> formLogin.disable());
+        http.httpBasic(httpBasic -> httpBasic.disable());
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .requestMatchers("/api/s/**").authenticated()
+                        .requestMatchers("api/admin/**").hasRole(UserRole.ADMIN.toString())
+                        .anyRequest().permitAll()
+        );
+        return http.build();
+
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        logger.debug("debug: configurationSource cors 설정이 SecurityFilterChain 에 등록됨");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+}
