27e19e880bcd95ad29bf996a6521544b8375c304
pxvnc1617 <chsong1617@nate.com>
2024-05-24 12:35:57 +0900

    [fix] append validationApiException

--- src/main/java/com/example/bank_coding/exceptionHandler/CustomExceptionHandler.java
+++ src/main/java/com/example/bank_coding/exceptionHandler/CustomExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.bank_coding.exceptionHandler;
 
 import com.example.bank_coding.dto.ResponseDto;
+import com.example.bank_coding.exceptionHandler.ex.CustomValidationException;
 import com.example.bank_coding.exceptionHandler.ex.CustomApiException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,9 +16,15 @@ public class CustomExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> validationApiException(CustomApiException customApiException) {
+        logger.error(customApiException.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, customApiException.getMessage(), customApiException.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomApiException.class)
     public ResponseEntity<?> apiException(CustomApiException customApiException) {
         logger.error(customApiException.getMessage());
-        return new ResponseEntity<>(new ResponseDto<>(-1, customApiException.getMessage(), null), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ResponseDto<>(-1, customApiException.getMessage(), customApiException), HttpStatus.BAD_REQUEST);
     }
 
 }
